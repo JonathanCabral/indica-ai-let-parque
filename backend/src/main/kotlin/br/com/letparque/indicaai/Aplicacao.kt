aa9573824e632ec1435606bcf1c330a24e0ce820
Jonathan Cabral <dev.jonathancabral@gmail.com>
2025-06-28 17:24:17 -0300

    config inicial backend

---
+++ backend/src/main/kotlin/br/com/letparque/indicaai/Aplicacao.kt
@@ -0,0 +1,11 @@
+package br.com.letparque.indicaai
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IndicaAiLetParqueApplication
+
+fun main(args: Array<String>) {
+	runApplication<IndicaAiLetParqueApplication>(*args)
+}
---
+++ backend/src/test/kotlin/br/com/letparque/indicaai/IndicaAiLetParqueApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.letparque.indicaai
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IndicaAiLetParqueApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
